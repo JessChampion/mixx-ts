090f553bb3367fec206dd02dde35f9c1cadb125c
Jess Champion <jess.e.champion@gmail.com>
2017-05-28 18:31:11 +1200

    Setup post css

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './index.pcss';
 
 ReactDOM.render(
   <App />,
